9e4268105c8af302ff164ac75c5c7cde678ce497
Payton Harris <paytonharris000@gmail.com>
2021-10-19 23:35:59 -0400

    update the homepage text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        payton
+        Testing deployment to amplify
       </header>
     </div>
   );
